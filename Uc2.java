9ac019b53b41548048412a1192aa2d1ea93ad4ca
anand10062000 <anandkumarramisetti@gmail.com>
2021-09-15 16:00:51 +0530

    attendance

---
+++ Uc2.java
@@ -0,0 +1,19 @@
+
+public class Uc2 {
+
+	public static void main(String[] args) {
+		Random ran = new Random();
+		int n=ran.nextInt(2);
+		System.out.println(n);
+		if(n==0) 
+			System.out.println("obsent");
+		else 
+			System.out.println("prsent");
+			
+		
+			
+ 
+
+	}
+
+}
